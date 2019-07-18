22be3e62850c5bc01957c6cfb4c48c517735f88c
Jose L Sanchez <jl_sanche@mac.com>
2019-07-18 12:33:36 -0700

    implemented ajax call for current playing album artwork

--- includes/nowPlayingBarContainer.php
+++ includes/nowPlayingBarContainer.php
@@ -34,6 +34,13 @@ function setTrack(trackId, newPlaylist, play) {
 		});
 
 
+		$.post("includes/handlers/ajax/getAlbumJson.php", { albumId: track.album }, function(data) {
+			var album = JSON.parse(data);
+
+			$(".albumLink img").attr("src", album.artworkPath);
+		});
+
+
 		audioElement.setTrack(track.path);
 		audioElement.play();
 	});
@@ -65,7 +72,7 @@ function pauseSong() {
 		<div id="nowPlayingLeft">
 			<div class="content">
 				<span class="albumLink">
-					<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+					<img src="" class="albumArtwork">
 				</span>
 
 				<div class="trackInfo">
